07b05d19ca96e294b80f0b1c1bb54fb290b7898a
Matthias Stöckli <matthias.stoeckli@flowable.com>
2023-04-30 10:23:14 +0200

    env var test

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const myEnvVar = process.env["MY_ENV_VAR"];
+
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`${myEnvVar}!`);
     await sleep(5000);
   }
 }
